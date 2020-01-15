7abb8725655dc7d0f39bd9e3d538fdc2d1253c87
Sebastian <durhams199@potsdam.edu>
2020-01-14 21:53:56 -0500

    added more unit tests

--- Test.py
+++ Test.py
@@ -23,5 +23,34 @@ class TestMatrixConstruction(unittest.TestCase):
     def test_set_row_values(self):
         self.assertEqual(self.test_matrix.matrix[0], [1,2])
 
+    def test_set_row_errors(self):
+        self.assertRaises(IndexError, self.test_matrix.set_row, 5, [])
+        self.assertRaises(IndexError, self.test_matrix.set_row, 5, [])
+        self.assertRaises(ValueError, self.test_matrix.set_row, 1, 6)
+
+class TestMatrixMethods(unittest.TestCase):
+    def setUp(self):
+        self.test_matrix_1 = Matrix(3,3)
+        self.test_matrix_1.set_row(1, [1,2,3])
+        self.test_matrix_1.set_row(2, [4,5,6])
+        self.test_matrix_1.set_row(3, [7,8,9])
+
+        self.test_matrix_2 = Matrix(3,3)
+        self.test_matrix_2.set_row(1, [1,1,1])
+        self.test_matrix_2.set_row(2, [1,1,1])
+        self.test_matrix_2.set_row(3, [1,1,1])
+
+    def test_matrix_addition(self):
+        sum_matrix = Matrix(3,3)
+        sum_matrix.set_row(1, [2,3,4])
+        sum_matrix.set_row(2, [5,6,7])
+        sum_matrix.set_row(3, [8,9,10])
+
+        self.assertEqual(self.test_matrix_1.add_matrix(self.test_matrix_2).matrix,
+                         sum_matrix.matrix)
+
+        self.assertEqual(self.test_matrix_2.add_matrix(self.test_matrix_1).matrix,
+                         sum_matrix.matrix)
+
 if __name__ == '__main__':
     unittest.main()
